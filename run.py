cf6ceebcf421666d45e0c5ef7b0a51eacc510e17
GroundBuilder <erik_rynnes@hotmail.com>
2023-03-06 14:51:45 +0000

    Collect sales data for last 5 markets in list of columns.

--- run.py
+++ run.py
@@ -58,9 +58,9 @@ def validate_data(values):
 These have been refactored to a singel update_worksheet.
 
 def update_sales_worksheet(data):
-    """
+"""
 #    Update sales worksheet, add new row with the list data provided.
-    """
+"""
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
@@ -68,9 +68,9 @@ def update_sales_worksheet(data):
 
 
 def update_surplus_worksheet(data):
-    """
+"""
 #    Update surplus worksheet, add new row to the list surplus provided.
-    """
+"""
     print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
@@ -109,6 +109,19 @@ def calculate_surplus_data(sales_now):
     
     return surplus_data
 
+def get_last_5_enteries_sales():
+    """
+    Collects collumns of data from sales worksheeet, collecting
+    the last 5 enteries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
 
 def main():
     """
@@ -123,4 +136,6 @@ def main():
 
 
 print("Welcome to Love Sanwiches Data Automation\n")
-main()
+# main()
+
+sales_columns = get_last_5_enteries_sales()
